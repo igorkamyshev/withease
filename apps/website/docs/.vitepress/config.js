3a2ac659e8baecdc2740ddc2ceaaa385cddfdf54
Igor Kamyshev <igor@kamyshev.me>
2023-02-24 10:25:53 +0700

    Add link on site

--- apps/website/docs/.vitepress/config.js
+++ apps/website/docs/.vitepress/config.js
@@ -1,61 +1,67 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  lang: "en-US",
-  title: "With Ease",
+  lang: 'en-US',
+  title: 'With Ease',
   description:
-    "A set of libraries and recipes to make frontend development easier thanks to Effector",
+    'A set of libraries and recipes to make frontend development easier thanks to Effector',
   lastUpdated: true,
-  outDir: "../../../dist/apps/website",
+  outDir: '../../../dist/apps/website',
   head: [],
   themeConfig: {
-    siteTitle: "With Ease",
+    siteTitle: 'With Ease',
     footer: {
-      message: "Released under the MIT License.",
-      copyright: "Copyright © 2023-present Igor Kamyşev",
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present Igor Kamyşev',
     },
     socialLinks: [
-      { icon: "github", link: "https://github.com/igorkamyshev/withease" },
-      { icon: "twitter", link: "https://twitter.com/EffectorJS" },
-      { icon: "discord", link: "https://discord.gg/zU9NDR5GpM" },
+      { icon: 'github', link: 'https://github.com/igorkamyshev/withease' },
+      { icon: 'twitter', link: 'https://twitter.com/EffectorJS' },
+      { icon: 'discord', link: 'https://discord.gg/zU9NDR5GpM' },
     ],
     editLink: {
       pattern:
-        "https://github.com/igorkamyshev/withease/edit/master/apps/website/docs/:path",
+        'https://github.com/igorkamyshev/withease/edit/master/apps/website/docs/:path',
     },
     nav: [
-      { text: "Packages", items: [{ text: "i18next", link: "/i18next/" }] },
-      { text: "Magazine", link: "/magazine/" },
       {
-        text: "More",
-        items: [{ text: "Effector", link: "https://effector.dev" }],
+        text: 'Packages',
+        items: [
+          { text: 'i18next', link: '/i18next/' },
+          { text: 'web-api', link: '/web-api/' },
+        ],
+      },
+      { text: 'Magazine', link: '/magazine/' },
+      {
+        text: 'More',
+        items: [{ text: 'Effector', link: 'https://effector.dev' }],
       },
     ],
     sidebar: {
-      "/magazine/": [
+      '/magazine/': [
         {
-          text: "Architecture",
+          text: 'Architecture',
           items: [
             {
-              text: "Global variables",
-              link: "/magazine/global_variables",
+              text: 'Global variables',
+              link: '/magazine/global_variables',
             },
             {
-              text: "Explicit start of the app",
-              link: "/magazine/explicit_start",
+              text: 'Explicit start of the app',
+              link: '/magazine/explicit_start',
             },
             {
-              text: "Dependency injection",
-              link: "/magazine/dependency_injection",
+              text: 'Dependency injection',
+              link: '/magazine/dependency_injection',
             },
           ],
         },
         {
-          text: "Effector deep dive",
+          text: 'Effector deep dive',
           items: [
             {
-              text: "Fork API rules",
-              link: "/magazine/fork_api_rules",
+              text: 'Fork API rules',
+              link: '/magazine/fork_api_rules',
             },
           ],
         },
