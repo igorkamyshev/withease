259b9f6a847b02641be8a8fd55cab8ee8f9e24b9
Igor Kamyshev <igor@kamyshev.me>
2023-06-22 14:42:50 +0700

    Fix typo

--- apps/website/docs/web-api/apis.data.ts
+++ apps/website/docs/web-api/apis.data.ts
@@ -4,11 +4,7 @@ const config: SiteConfig = (globalThis as any).VITEPRESS_CONFIG;
 
 export default {
   load() {
-    console.log(config.userConfig.themeConfig.sidebar['/web-api/']);
-    const apis = config.userConfig.themeConfig.sidebar['/web-api/']
-      .at(0)
-      .items.at(1).items;
-
-    return apis;
+    return config.userConfig.themeConfig.sidebar['/web-api/'].at(0).items.at(1)
+      .items;
   },
 };
