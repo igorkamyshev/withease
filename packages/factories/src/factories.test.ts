2a223751881fd00433d3a604b700b3eb8fa677f3
Igor Kamyshev <igor@kamyshev.me>
2023-07-05 21:59:13 +0700

    improve tests

--- packages/factories/src/factories.test.ts
+++ packages/factories/src/factories.test.ts
@@ -5,12 +5,19 @@ import { invoke } from './invoke';
 
 describe('factories', () => {
   test('invoke calls original creator with passed params', () => {
-    const creator = vi.fn();
-    const factory = createFactory(creator);
-    const params = [1, 2, 3];
+    const mock = vi.fn();
+    const factory = createFactory((params: number[]) => {
+      mock(params);
 
-    invoke(factory, params);
+      return params.at(0);
+    });
 
-    expect(creator).toHaveBeenCalledWith(params);
+    const valueOne = invoke(factory, [1, 2, 3]);
+    expect(mock).toHaveBeenCalledWith([1, 2, 3]);
+    expect(valueOne).toBe(1);
+
+    const valueTwo = invoke(factory, [2, 3, 4]);
+    expect(mock).toHaveBeenCalledWith([2, 3, 4]);
+    expect(valueTwo).toBe(2);
   });
 });
