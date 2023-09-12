e7874fbedb88e7544b84031fd820b121fe338a74
Igor Kamyshev <igor@kamyshev.me>
2023-09-12 15:46:02 +0700

    Fixes

--- apps/website/docs/.vitepress/theme/LiveDemo.vue
+++ apps/website/docs/.vitepress/theme/LiveDemo.vue
@@ -2,19 +2,19 @@
 import webApiRaw from '../../../../../dist/packages/web-api?raw';
 import { Sandpack } from 'sandpack-vue3';
 
-const props = defineProps(['demoFile'])
+const props = defineProps(['demoFile']);
 
 const files = {
-  "/src/App.vue": props.demoFile,
-  ...localPackage({ name: "web-api", content: webApiRaw }),
-}
+  '/src/App.vue': props.demoFile,
+  ...localPackage({ name: 'web-api', content: webApiRaw }),
+};
 
 const customSetup = {
   dependencies: {
-    "effector": "latest",
-    "effector-vue": "latest"
-  }
-}
+    effector: 'latest',
+    'effector-vue': 'latest',
+  },
+};
 
 function localPackage({ name, content }) {
   return {
@@ -22,17 +22,22 @@ function localPackage({ name, content }) {
       hidden: true,
       code: JSON.stringify({
         name: `@withease/${name}`,
-        main: "./index.js",
+        main: './index.js',
       }),
     },
     [`/node_modules/@withease/${name}/index.js`]: {
       hidden: true,
       code: content,
     },
-  }
+  };
 }
 </script>
 
 <template>
-  <Sandpack template="vue3" theme="auto" :files="files" :customSetup="customSetup" />
+  <Sandpack
+    template="vue3"
+    theme="auto"
+    :files="files"
+    :customSetup="customSetup"
+  />
 </template>
