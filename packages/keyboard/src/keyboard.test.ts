defb20eea5f07c80d89175534ca8d7e766be5e82
Igor Kamyshev <igor@kamyshev.me>
2023-09-16 15:03:30 +0700

    Add tests from older PR

--- packages/keyboard/src/keyboard.test.ts
+++ packages/keyboard/src/keyboard.test.ts
@@ -11,29 +11,53 @@ const teardown = createEvent();
 
 const keyboard = trackKeyboard({ setup, teardown });
 
-describe('sequence', () => {
-  test('trigger after typing', async () => {
-    const listener = vi.fn();
+/*
+ * Tests cannot be .concurrent because jsdom
+ */
+describe('trackKeyboard', () => {
+  describe('sequence', () => {
+    test('trigger after typing', async () => {
+      const listener = vi.fn();
 
-    const typed = keyboard.sequence('iddqd');
+      const typed = keyboard.sequence('iddqd');
 
-    const user = userEvent.setup();
+      const user = userEvent.setup();
 
-    const scope = fork();
+      const scope = fork();
 
-    createWatch({ unit: typed, fn: listener, scope });
+      createWatch({ unit: typed, fn: listener, scope });
 
-    await allSettled(setup, { scope });
+      await allSettled(setup, { scope });
 
-    await user.keyboard('iddqd');
-    expect(listener).toBeCalledTimes(1);
+      await user.keyboard('iddqd');
+      expect(listener).toBeCalledTimes(1);
 
-    await user.keyboard('iddqd');
-    expect(listener).toBeCalledTimes(2);
+      await user.keyboard('iddqd');
+      expect(listener).toBeCalledTimes(2);
 
-    await allSettled(teardown, { scope });
+      await allSettled(teardown, { scope });
 
-    await user.keyboard('iddqd');
-    expect(listener).toBeCalledTimes(2);
+      await user.keyboard('iddqd');
+      expect(listener).toBeCalledTimes(2);
+    });
+
+    test('DO NOT trigger after random typing', async () => {
+      const start = createEvent();
+      const typed = keyboard.sequence('randomstring');
+
+      const listener = vi.fn();
+
+      const user = userEvent.setup();
+
+      const scope = fork();
+
+      createWatch({ unit: typed, fn: listener, scope });
+
+      await allSettled(start, { scope });
+
+      await user.keyboard('iddqd');
+
+      expect(listener).not.toHaveBeenCalled();
+    });
   });
 });
